71a368723c02cd72a101bf9b4dba692c3fd40df8
jaehyun <0908jhk@naver.com>
2024-03-17 18:16:08 +0900

    pull이 안되서 직접 푸쉬

--- src/components/Subject/SubjectTable.js
+++ src/components/Subject/SubjectTable.js
@@ -23,22 +23,22 @@ const SubjectTable = () => {
 
   useEffect(() => {
     axios
-      .get(`http://localhost:8080/subject`)
-      .then((response) => {
-        setSubjects(response.data.results.subject);
-        setAllSubjects(response.data.results.subject);
-      })
-      .catch((error) => {
-        console.error("Error fetching data: ", error);
-        setSubjects([]);
-      });
+        .get(`http://localhost:8080/subject`)
+        .then((response) => {
+          setSubjects(response.data.results.subject);
+          setAllSubjects(response.data.results.subject);
+        })
+        .catch((error) => {
+          console.error("Error fetching data: ", error);
+          setSubjects([]);
+        });
   }, []);
 
   const pageNumbers = [];
   for (
-    let i = startPage;
-    i < startPage + 5 && i <= Math.ceil(subjects.length / itemsPerPage);
-    i++
+      let i = startPage;
+      i < startPage + 5 && i <= Math.ceil(subjects.length / itemsPerPage);
+      i++
   ) {
     pageNumbers.push(i);
   }
@@ -46,14 +46,14 @@ const SubjectTable = () => {
     let filteredSubjects = allSubjects;
     if (semester !== "" && semester !== "--학기-- 선택") {
       filteredSubjects = filteredSubjects.filter(
-        (subject) => subject.subjectOpen === semester
+          (subject) => subject.subjectOpen === semester
       );
     }
     if (search !== "") {
       filteredSubjects = filteredSubjects.filter(
-        (subject) =>
-          subject.subjectName.includes(search) ||
-          subject.subjectProfessor.includes(search)
+          (subject) =>
+              subject.subjectName.includes(search) ||
+              subject.subjectProfessor.includes(search)
       );
     }
     setSubjects(filteredSubjects);
@@ -64,7 +64,7 @@ const SubjectTable = () => {
       setModalData("상세 정보를 불러오는 중입니다.");
     } else {
       const subjectDetail = subjectDetailData.find(
-        (subject) => subject.subjectId === subjectId
+          (subject) => subject.subjectId === subjectId
       );
       if (subjectDetail) {
         setModalData(subjectDetail.subjectGoal);
@@ -75,27 +75,27 @@ const SubjectTable = () => {
   };
 
   return (
-    <div className="container-form">
-      <div>
-        <label className="label-title">컴퓨터공학과</label>
-        <input
-          className="search-form"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-          placeholder="강좌명 또는 교수명 검색"
-        />
-      </div>
-      <div className="semester-form">
-        <select value={semester} onChange={(e) => setSemester(e.target.value)}>
-          <option value="">--학기-- 선택</option>
-          <option value="2023-1">2023년 1학기</option>
-          <option value="2023-2">2023년 2학기</option>
-          <option value="2024-1">2024년 1학기</option>
-        </select>
-      </div>
+      <div className="container-form">
+        <div>
+          <label className="label-title">컴퓨터공학과</label>
+          <input
+              className="search-form"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              placeholder="강좌명 또는 교수명 검색"
+          />
+        </div>
+        <div className="semester-form">
+          <select value={semester} onChange={(e) => setSemester(e.target.value)}>
+            <option value="">--학기-- 선택</option>
+            <option value="2023-1">2023년 1학기</option>
+            <option value="2023-2">2023년 2학기</option>
+            <option value="2024-1">2024년 1학기</option>
+          </select>
+        </div>
 
-      <table className="SubjectTable">
-        <thead>
+        <table className="SubjectTable">
+          <thead>
           <tr>
             <th>학년</th>
             <th>구분</th>
@@ -104,60 +104,60 @@ const SubjectTable = () => {
             <th>교수명</th>
             <th>Status</th>
           </tr>
-        </thead>
-        <tbody>
+          </thead>
+          <tbody>
           {currentItems.map((subject, index) => (
-            <tr key={index}>
-              <td>{subject.subjectGrade}학년</td>
-              <td>{subject.subjectSort}</td>
-              <td
-                onClick={() => {
-                  setModalContent(subject.subjectId);
-                  setIsModalOpen(true);
-                }}
-              >
-                {subject.subjectName}
-              </td>
-              <td>{subject.subjectOpen}</td>
-              <td>{subject.subjectProfessor}</td>
-              <td>{subject.subjectStatus}</td>
-            </tr>
+              <tr key={index}>
+                <td>{subject.subjectGrade}학년</td>
+                <td>{subject.subjectSort}</td>
+                <td
+                    onClick={() => {
+                      setModalContent(subject.subjectId);
+                      setIsModalOpen(true);
+                    }}
+                >
+                  {subject.subjectName}
+                </td>
+                <td>{subject.subjectOpen}</td>
+                <td>{subject.subjectProfessor}</td>
+                <td>{subject.subjectStatus}</td>
+              </tr>
           ))}
-        </tbody>
-        <div className="pageNation-container">
-          {startPage >= 0 && (
-            <button
-              onClick={() =>
-                setStartPage(startPage - 4 >= 1 ? startPage - 4 : 1)
-              }
-              disabled={startPage === 1}
-            >
-              {"<"}
-            </button>
-          )}
-          {pageNumbers.map((number) => (
-            <button
-              key={number}
-              onClick={() => setCurrentPage(number)}
-              className={currentPage === number ? "active" : ""}
-            >
-              {number}
-            </button>
-          ))}
-          {startPage + 4 < Math.ceil(subjects.length / itemsPerPage) && (
-            <button onClick={() => setStartPage(startPage + 4)}>{">"}</button>
-          )}
-        </div>
-      </table>
-      {isModalOpen && (
-        <div className="modal" onClick={() => setIsModalOpen(false)}>
-          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-            <span onClick={() => setIsModalOpen(false)}>Close</span>
-            <p>{modalContent}</p>
+          </tbody>
+          <div className="pageNation-container">
+            {startPage >= 0 && (
+                <button
+                    onClick={() =>
+                        setStartPage(startPage - 4 >= 1 ? startPage - 4 : 1)
+                    }
+                    disabled={startPage === 1}
+                >
+                  {"<"}
+                </button>
+            )}
+            {pageNumbers.map((number) => (
+                <button
+                    key={number}
+                    onClick={() => setCurrentPage(number)}
+                    className={currentPage === number ? "active" : ""}
+                >
+                  {number}
+                </button>
+            ))}
+            {startPage + 4 < Math.ceil(subjects.length / itemsPerPage) && (
+                <button onClick={() => setStartPage(startPage + 4)}>{">"}</button>
+            )}
           </div>
-        </div>
-      )}
-    </div>
+        </table>
+        {isModalOpen && (
+            <div className="modal" onClick={() => setIsModalOpen(false)}>
+              <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+                <span onClick={() => setIsModalOpen(false)}>Close</span>
+                <p>{modalContent}</p>
+              </div>
+            </div>
+        )}
+      </div>
   );
 };
 
